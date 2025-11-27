dd556fa8bfc0cba5805c4e117f9ebbd1fe41e639
uedake <uedakensuke722@gmail.com>
2025-11-27 13:50:55 +0900

    set livekit config

--- cdk/spec/sample-api/src_lambda/livekit_token/src/api/util/config.py
+++ cdk/spec/sample-api/src_lambda/livekit_token/src/api/util/config.py
@@ -1,5 +1,16 @@
 # 公開のレポジトリにpushする場合は、ここに接続情報を記載しないこと
+# 現在main.dev,testで同じAPI keyを使用しているが今後分けるべき
 LIVEKIT_ENV = {
+    "main": {
+        "LIVEKIT_API_KEY": "APItp6X3V3mBBSi",
+        "LIVEKIT_API_SECRET": "F7tWc0ZT95GYWIf92XkRrpfVldWAQPWC6yD4X95Gl3E",
+        "LIVEKIT_URL": "wss://test-rh5n4iee.livekit.cloud",
+    },
+    "dev": {
+        "LIVEKIT_API_KEY": "APItp6X3V3mBBSi",
+        "LIVEKIT_API_SECRET": "F7tWc0ZT95GYWIf92XkRrpfVldWAQPWC6yD4X95Gl3E",
+        "LIVEKIT_URL": "wss://test-rh5n4iee.livekit.cloud",
+    },
     "test": {
         "LIVEKIT_API_KEY": "APItp6X3V3mBBSi",
         "LIVEKIT_API_SECRET": "F7tWc0ZT95GYWIf92XkRrpfVldWAQPWC6yD4X95Gl3E",
